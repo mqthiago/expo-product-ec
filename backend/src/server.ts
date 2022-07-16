b9ee5c436fc025a28fe27e732bb5ce7aea309024
Thiago <thiago.poison@gmail.com>
2022-07-16 20:00:40 -0300

    lintado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,28 +1,29 @@
-import express, { request, response } from 'express'
-import { PrismaClient, Product } from '@prisma/client'
-const prisma = new PrismaClient()
-const server = express()
+/* eslint-disable no-console */
+import express from 'express';
+import { PrismaClient, Product } from '@prisma/client';
 
-server.use(express.json())
-server.get('/product',async(request,response)=>{
-    const products = await prisma.product.findMany()
-    console.log(products)    
-    return response.json(products)
-})
-server.post('/product',async(request,response)=>{
-    console.log(request.body)
-    const {description,imageUrl}=request.body as Product
-    const product = await prisma.product.create({data:{description,imageUrl}})
-    return response.status(201).json(product)
-})
+const prisma = new PrismaClient();
+const server = express();
 
-server.delete('/product/:id',async(request,response)=>{
-    const {id}=request.params
-    await prisma.product.delete({where:{id:parseInt(id)}})
-    return response.status(204).json()
-})
+server.use(express.json());
+server.get('/product', async (request, response) => {
+  const products = await prisma.product.findMany();
+  console.log(products);
+  return response.json(products);
+});
+server.post('/product', async (request, response) => {
+  console.log(request.body);
+  const { description, imageUrl } = request.body as Product;
+  const product = await prisma.product.create({ data: { description, imageUrl } });
+  return response.status(201).json(product);
+});
 
-server.listen(3333,()=>{
-    console.log('Agora foi')
-})
+server.delete('/product/:id', async (request, response) => {
+  const { id } = request.params;
+  await prisma.product.delete({ where: { id: parseInt(id, 10) } });
+  return response.status(204).json();
+});
 
+server.listen(3333, () => {
+  console.log('Agora foi');
+});
